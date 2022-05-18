c87aaf728b459c5c62f5506ca6e4414f225fdb01
Alex Aghajanyan <aghajanyanalexander@gmail.com>
2022-05-18 22:51:50 +0400

    fix coordinate calculation for simple harmonic motion

--- spring.java
+++ spring.java
@@ -24,27 +24,21 @@ public class Spring {
         this.k = k;
     }
 
-    private Double[] get_coordinates(double e, double t, double dt) {
-        // Calculating the apmlitude.
-        double ampl = Math.sqrt((2 * e) / this.k);
-
-        // Calculate maximum velocity.
-        double max_v = ampl*Math.sqrt(this.k);
-
-        // Calculating oscillation period.
-        double period = Math.PI*2*ampl / max_v;
+    private Double[] get_coordinates(double t0, double t1, double dt, double x0, double v0, double m) {
 
         // Calculating the angular velocity.
-        double omega = 2*Math.PI / period;
+        double omega = Math.sqrt(this.k / m);
 
-        double timestep = t / dt; 
+        double timestep = (t1 - t0) / dt; 
         Double[] x_coordinates = new Double[(int)timestep]; 
 
-        // Computing the coordinates per each dt starting from t = 0.
-        for(double i = 0; i <= t; i += dt) {
+        // Computing the coordinates per each dt starting from t = t0.
+        for(double i = t0; i <= t1; i += dt) {
             int count = 0;
-            System.out.println(Math.cos(omega*i));
-            x_coordinates[count] = ampl * Math.cos(omega*i);
+
+            // Obtaining coordinates of object-spring system.
+            x_coordinates[count] = x0 * Math.cos(omega*i) + (v0/omega) * Math.sin(omega*i);
+            System.out.println(x_coordinates[count]);
             count++;
         }
 
@@ -52,37 +46,19 @@ public class Spring {
     }
 
     public Double[] move(double t, double dt, double x0, double v0) {
-
-        // Calculating energy based on the given position and velocity.
-        double e = (Math.pow(x0, 2) * this.k)/2 + Math.pow(v0, 2)/2;   
-        System.out.println(e);
-
-        return get_coordinates(e, t, dt);
+        return get_coordinates(0, t, dt, x0, v0, 1);
     }
 
     public Double[] move(double t, double dt, double x0) {
-
-        // Calculating energy based on the given position and velocity.
-        double e = (Math.pow(x0, 2) / 2);   
-        System.out.println(e);
-
-        return get_coordinates(e, t, dt);
+        return get_coordinates(0, t, dt, x0, 0, 1);
     }
 
-    public Double[] move(double t0, double tl, double dt, double x0, double v0) {
-
-        // Calculating energy based on the given position and velocity.
-        double e = (Math.pow(x0, 2) * this.k)/2 + Math.pow(v0, 2)/2;  
-        double t = tl - t0;
-        return get_coordinates(e, t, dt);
+    public Double[] move(double t0, double t1, double dt, double x0, double v0) {
+        return get_coordinates(t0, t1, dt, x0, v0, 1);
     }
 
-    public Double[] move(double t0, double tl, double dt, double x0, double v0, double m) {
-
-        // Calculating energy based on the given position and velocity.
-        double e = (Math.pow(x0, 2) * this.k)/2 + Math.pow(v0, 2)*m/2;  
-        double t = tl - t0;
-        return get_coordinates(e, t, dt);
+    public Double[] move(double t0, double t1, double dt, double x0, double v0, double m) {
+        return get_coordinates(t0, t1, dt, x0, v0, m);
     }
 
     public Spring inSeries(Spring that) {
@@ -100,6 +76,7 @@ public class Spring {
     public static void main(String[] args) {
         Spring spring = new Spring(1.5);
         spring.move(1.0, 0.2, 1.0);
+        System.out.println();
         spring.move(1.0, 0.2, 1.0, 2.0);        
     }
 }
