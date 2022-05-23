08789835244816066b66f2bd1cd2fe2eb8f4df0f
Alex Aghajanyan <aghajanyanalexander@gmail.com>
2022-05-23 19:10:17 +0400

    converter

--- src/Converter.java
+++ src/Converter.java
@@ -1,5 +1,34 @@
 package src;
+import java.util.*;
 
 public class Converter {
 
+	public Spring generateSpringSystem(Integer[] values) {
+		List<Spring> springs = new ArrayList<>();
+		
+		String springExpr = "[";
+		
+		for(int i=0; i < values.length; i++) {
+			if(values[i] == 1) {
+				springs.add(new Spring(Math.pow(2, values.length - i - 1)));
+				springExpr += "[]";
+			}
+		}
+		
+		springExpr += "]";
+		
+		SpringArray arr = new SpringArray();
+		
+	    Spring[] springArray = new Spring[springs.size()];
+        springs.toArray(springArray);
+	      
+		Spring generated_spring = arr.equivalentSpring(springExpr, springArray);
+		
+		return generated_spring;
+	}
+	
+	public static Double[] getOscillation(Spring spring, int t0, int t1) {
+		double dt = (t1 - t0) / 100;
+		return spring.move(t1, t0, dt, 0, 1, 1);
+	}
 }
